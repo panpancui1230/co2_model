ec3bb3bf409033e004f791c0d40d1a91a0947025
pan-mba <panpancui6@gmail.com>
2024-11-12 22:14:59 +0800

    message

--- model/KEA3_lim.py
+++ model/KEA3_lim.py
@@ -593,7 +593,7 @@ def do_stuff(LIGHT):
         combined_df = pd.concat([combined_df, gtype_df], ignore_index=True)  # 累积到 combined_df
     
     # 保存所有 idx 的结果到一个 CSV 文件
-    combined_df.to_csv(f'./logs_new/combined_{LIGHT}_simulated_lim.csv', index=False)
+    combined_df.to_csv(f'./logs_lim/combined_{LIGHT}_simulated_lim.csv', index=False)
     print(f"All results for LIGHT {LIGHT} saved to combined_{LIGHT}_simulated_lim.csv")
     
 global FREQUENCY, LIGHT, T_ATP
