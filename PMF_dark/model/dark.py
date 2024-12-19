42d567709fc6331525373f24e29fdef0f6a26061
pan-mba <panpancui6@gmail.com>
2024-12-19 17:18:10 +0800

    f

--- PMF_dark/model/dark.py
+++ PMF_dark/model/dark.py
@@ -24,58 +24,70 @@ from utils import sim_constants
 from calc import block
 # from sun_sim import sunshine
 
-def f(t, y, pKreg, max_b6f, lumen_protons_per_turnover, ATP_synthase_max_turnover, Volts_per_charge, perm_K,
-      n, Em7_PQH2, Em7_PC, Em_Fd, buffering_capacity, k_Fd_to_NADP, k_CBC, k_KEA, k_VCCN1, k_CLCE, k_NDH):
+def f(t, y, lumen_protons_per_turnover, ATP_synthase_max_turnover, Volts_per_charge, perm_K,
+      n, buffering_capacity, k_KEA, k_VCCN1, k_CLCE):
     computer = block()
 
-    Dy, pHstroma, pHlumen, pHlumen, pmf, Klumen, Kstroma, NADPH_pool, NADP_pool, Cl_lumen, Cl_stroma, Hstroma=y
+    Hin,pHlumen, Dy, pmf, Klumen, Kstroma, Cl_lumen, Cl_stroma, Hstroma, pHstroma=y
 
+    #pmf计算
     pmf=Dy + 0.06*(pHstroma-pHlumen)
-    
+
+    #KEA3
     Hlumen = 10**(-1*pHlumen)
     Hstroma = 10**(-1*pHstroma)
-    activity = computer.ATP_synthase_actvt(t, T_ATP)
-
-    d_protons_to_ATP = computer.Vproton_pmf_actvt(pmf, activity, ATP_synthase_max_turnover, n)
-    d_H_ATP_or_passive = computer.V_H_light(light_per_L, d_protons_to_ATP, pmf, Hlumen)                              
-
-    d_ATP_made=d_protons_to_ATP/n 
-    
-    d_ATP_consumed = d_ATP_made
-
-    net_protons_in =  - d_H_ATP_or_passive
-    
     v_KEA = k_KEA*(Hlumen*Kstroma -  Hstroma*Klumen)
    
+    #V_K
     K_deltaG=-0.06*np.log10(Kstroma/Klumen) + Dy
     v_K_channel = perm_K * K_deltaG*(Klumen+Kstroma)/2
     
-    net_Klumen =  v_KEA - v_K_channel        
-    dKlumen = net_Klumen*lumen_protons_per_turnover   
-    dKstroma=0
-    
+    #VCCN1
     driving_force_Cl = 0.06* np.log10(Cl_stroma/Cl_lumen) + Dy
-    v_VCCN1 = k_VCCN1 * computer.Cl_flux_relative(driving_force_Cl) * (Cl_stroma + Cl_lumen)/2#Cl_flux_relative()
+    v_VCCN1 = k_VCCN1 * computer.Cl_flux_relative(driving_force_Cl) * (Cl_stroma + Cl_lumen)/2
 
-    v_CLCE =  k_CLCE*(driving_force_Cl*2+pmf)*(Cl_stroma + Cl_lumen)*(Hlumen+Hstroma)/4    
+    #CLCE
+    v_CLCE =  k_CLCE*(driving_force_Cl*2+pmf)*(Cl_stroma + Cl_lumen)*(Hlumen+Hstroma)/4   
+
+    #dK+/dt
+    net_Klumen =  v_KEA - v_K_channel        
+    dKlumen = net_Klumen*lumen_protons_per_turnover  
+    dKstroma=0
+
+    #dCl-/dt
     net_Cl_lumen_in = v_VCCN1 + 2*v_CLCE
     dCl_lumen = net_Cl_lumen_in * lumen_protons_per_turnover
     dCl_stroma = -0.1*dCl_lumen
-    
+
+    # dATP
+    # activity = computer.ATP_synthase_actvt(t, T_ATP)
+    activity = computer.ATP_synthase_actvt(t, 300)
+    d_protons_to_ATP = computer.Vproton_pmf_actvt(pmf, activity, ATP_synthase_max_turnover, n)
+    d_ATP_made=d_protons_to_ATP/n 
+    d_ATP_consumed = d_ATP_made
+    dATP_pool= d_ATP_made - d_ATP_consumed
+    dADP_pool= - dATP_pool
+
+    #dpHlumen
+    # d_H_ATP_or_passive = computer.V_H_light(light_per_L, d_protons_to_ATP, pmf, Hlumen)      
+    d_H_ATP_or_passive = computer.V_H_light(d_protons_to_ATP, pmf, Hlumen)                         
+    net_protons_in =  - d_H_ATP_or_passive
     dHin = (net_protons_in - v_KEA - v_CLCE)*lumen_protons_per_turnover
     dpHlumen= -1*dHin / buffering_capacity 
 
+    #dpHstroma
     dHstroma = 0
-    
     dpHstroma = -1*dHstroma / buffering_capacity
+
+    #dDy
     delta_charges= - v_K_channel - v_VCCN1-3*v_CLCE 
-    
     dDy=delta_charges*Volts_per_charge
+
+    #dpmf
     dpmf= 0.06* dpHlumen + dDy
  
-    dATP_pool= d_ATP_made - d_ATP_consumed
-    dADP_pool= - dATP_pool
-    ddeltaGatp = 0
-    return [ dHin, dpHlumen, dDy, dpmf, ddeltaGatp, dKlumen, dKstroma, 
-            d_ATP_made, dATP_pool, dADP_pool, dCl_lumen, dCl_stroma,dHstroma, dpHstroma]
+    # return [dpHlumen, dDy, dpmf, dKlumen, dKstroma, dCl_lumen, dCl_stroma, dHstroma, dpHstroma]
 
+    return [dHin, dpHlumen, dDy, dpmf, dKlumen, dKstroma, 
+            d_ATP_made, dATP_pool, dADP_pool, dCl_lumen, dCl_stroma,dHstroma, dpHstroma]
+# pHlumen, Dy, pmf, Klumen, Kstroma, Cl_lumen, Cl_stroma, Hstroma, pHstroma=y
